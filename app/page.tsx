516bd33a8b8176a259a1187377d8352b956ab018
yudhaeric <yudhaericpamungkas@gmail.com>
2023-11-21 16:47:39 +0700

    Update Header

--- app/page.tsx
+++ app/page.tsx
@@ -2,40 +2,42 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='w-[90%] desktop:w-[80%] mx-auto'>
-      <header className='flex justify-between items-center w-full h-[70px] mb-10 border-b-1 border-black desktop:mb-5'>
-        <h1 className='pt-2 font-jomhuria text-[50px] desktop:text-[64px]'>Yudha<span className='text-[#18A3DF]'>.</span></h1>
-        <div className='flex gap-5 items-center desktop:gap-10'>
-          <a href="mailto:yudhaericpamungkas@gmail.com" target='_blank'>
-            <Image
-              className='desktop:w-30 desktop:h-30'
-              src="/images/icons/gmail.png"
-              alt="gmail"
-              width={33}
-              height={33}
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/yudha-eric-pamungkas-90a85a209/" target='_blank'>
-            <Image
-              src="/images/icons/linkedin.png"
-              alt="linkedin"  
-              width={30}
-              height={30}
-            />
-          </a>
-          <a href="https://github.com/yudhaeric" target='_blank'>
-            <Image
-              className='desktop:w-30 desktop:h-30'
-              src="/images/icons/github.png"
-              alt="github"
-              width={26}
-              height={26}
-            />
-          </a>
+    <main>
+      <header className='w-full fixed bg-[#FFF5EA] z-50'>
+        <div className='h-[70px] flex justify-between items-center mx-[5%] desktop:mx-[10%]'>
+          <h1 className='pt-2 font-jomhuria text-[50px] desktop:text-[64px]'>Yudha<span className='text-[#18A3DF]'>.</span></h1>
+          <div className='flex gap-5 items-center desktop:gap-10'>
+            <a href="mailto:yudhaericpamungkas@gmail.com" target='_blank'>
+              <Image
+                className='desktop:w-30 desktop:h-30'
+                src="/images/icons/gmail.png"
+                alt="gmail"
+                width={33}
+                height={33}
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/yudha-eric-pamungkas-90a85a209/" target='_blank'>
+              <Image
+                src="/images/icons/linkedin.png"
+                alt="linkedin"  
+                width={30}
+                height={30}
+              />
+            </a>
+            <a href="https://github.com/yudhaeric" target='_blank'>
+              <Image
+                className='desktop:w-30 desktop:h-30'
+                src="/images/icons/github.png"
+                alt="github"
+                width={26}
+                height={26}
+              />
+            </a>
+          </div>
         </div>
       </header>
-      <div className='desktop:mt-4'>
-        <div className='absolute right-[-100px] top-[-100px] desktop:inline mobile:hidden'>
+      <div className='pt-24 mx-[5%] desktop:mx-[10%]'>
+        <div className='absolute right-[-100px] top-[-50px] desktop:inline mobile:hidden'>
           <Image
             className='animate-spin-slow'
             src="/images/icons/shape.png"
@@ -88,6 +90,7 @@ export default function Home() {
           />
         </div>
       </div>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </main>
   )
 }
