ed6d18c113727dc4a6eea1a229cd1234e4a00c65
yudhaeric <yudhaericpamungkas@gmail.com>
2023-11-22 16:51:15 +0700

    Add Footer in Mobile

--- app/page.tsx
+++ app/page.tsx
@@ -145,6 +145,16 @@ export default function Home() {
         </div>
       </div>
       <div className='mt-[50%]'></div>
+      <footer className='w-full bg-[#18A3DF]'>
+        <div className='flex justify-between py-5 mx-[5%]'>
+          <div className='px-4 py-1 rounded-full border border-[#FFF5EA] text-[#FFF5EA]'>
+            <p className='text-[10px] font-monaMedium'>Portofolio</p>
+          </div>
+          <div className='px-4 py-1 rounded-full border border-[#FFF5EA] text-[#FFF5EA]'>
+            <p className='text-[10px] font-monaMedium'>Last Update: November 2023</p>
+          </div>
+        </div>
+      </footer>
     </main>
   )
 }
