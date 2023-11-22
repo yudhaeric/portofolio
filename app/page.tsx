06dd70b57fae647ce52a1c151f9b7208c8470c33
yudhaeric <yudhaericpamungkas@gmail.com>
2023-11-22 16:50:56 +0700

    Add Section About in Mobile

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <div className='pt-24 mx-[5%] desktop:mx-[10%]'>
+      <div className='pt-[50%] mx-[5%] desktop:mx-[10%] desktop:pt-[24px]'>
         <div className='absolute right-[-100px] top-[-50px] desktop:inline mobile:hidden'>
           <Image
             className='animate-spin-slow'
@@ -73,7 +73,7 @@ export default function Home() {
             height={22}
           />
         </div>
-        <div className='mt-5'>
+        <div className='mt-7'>
           <Image
             className='animate-spin-slow desktop:hidden'
             src="/images/icons/welcome.png"
@@ -90,7 +90,61 @@ export default function Home() {
           />
         </div>
       </div>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <div className='mt-[70%] mx-[5%]'>
+        <hr className='bg-[#9C9C9C] h-[1px]'></hr>
+        <div className='mt-3'>
+          <h3 className='font-anton uppercase text-[16px] text-[#9C9C9C]'>About Me</h3>
+          <h1 className='font-anton uppercase text-[30px] leading-9'>Exploring <br/> The <span className='text-[#18A3DF]'>Coder&apos;s</span> Identity</h1>
+          <div className='flex justify-center mt-5'>
+            <Image
+              src="/images/icons/shape-bg.png"
+              alt="shape"
+              width={100}
+              height={128}
+            />
+            <Image
+              className='absolute mt-7'
+              src="/images/picture.png"
+              alt="pictures"
+              width={93}
+              height={127}
+            />
+          </div>
+          <p className='text-[12px] mt-5 font-monaRegular'>
+            Hi there 👋 my full name is <span className='text-[#18A3DF] font-monaMedium'>Yudha Eric Pamungkas, </span>
+            with over one years, I&apos;ve evolved into a dedicated and 
+            <span className='font-monaMedium'> passionate web developer</span>. My commitment to 
+            staying up-to-date with the <span className='font-monaMedium'> latest web trends</span> and 
+            technologies, ensures that the websites I create 
+            are not only visually appealing but also 
+            <span className='font-monaMedium'> highly functional </span> and <span className='font-monaMedium'>responsive.</span>
+          </p>
+          <div className='flex gap-52'>
+            <div className='flex items-center mt-[-5px] ml-[-10px]'>
+              <p className='text-[8px] border border-[#1E1E1E] rounded-full px-[10px] py-[3px]'>6 Projects Done</p>
+              <Image
+                className='absolute mt-[-35px] left-[85px]'
+                src="/images/icons/arrow.png"
+                alt="pictures"
+                width={24}
+                height={15}
+              />
+            </div>
+            <a href="mailto:yudhaericpamungkas@gmail.com" target='_blank'>
+              <div className='flex flex-col items-center mt-[-5px]'>
+                <Image
+                  src="/images/icons/contact-arrow.png"
+                  alt="Contact"
+                  width={35}
+                  height={35}
+                />
+                <h3 className='text-[12px] font-monaMedium mt-[4px]'>Let&apos;s Talk!</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='mt-[50%]'></div>
     </main>
   )
 }
