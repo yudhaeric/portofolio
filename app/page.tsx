63afc16b447006efd233b257942aa2e037fd8c37
yudhaeric <yudhaericpamungkas@gmail.com>
2024-12-15 18:19:07 +0700

    Add New Animation Keyframes

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className='pt-[50%] mx-[5%] desktop:mx-[10%] desktop:pt-[75px]'>
           <div className='absolute right-[-100px] top-[-50px] desktop:inline mobile:hidden'>
             <Image
-              className='animate-spin-slow'
+              className='animate-spinSlow'
               src="/images/icons/shape.png"
               alt="line"
               width={250}
@@ -44,14 +44,14 @@ export default function Home() {
           </div>
           <div className='mt-7'>
             <Image
-              className='animate-spin-slow desktop:hidden'
+              className='animate-spinSlow desktop:hidden'
               src="/images/icons/welcome.png"
               alt="welcome"
               width={105}
               height={107}
             />
             <Image
-              className='animate-spin-slow desktop:inline mobile:hidden'
+              className='animate-spinSlow desktop:inline mobile:hidden'
               src="/images/icons/welcome.png"
               alt="welcome"
               width={125}
@@ -65,8 +65,8 @@ export default function Home() {
           <hr className='bg-[#9C9C9C] h-[1px] desktop:h-[2px]'></hr>
           <div className='flex gap-5'>
             <div className='desktop:w-[70%] mt-3'>
-              <h3 className='font-anton uppercase text-[16px] text-[#9C9C9C] desktop:text-[34px]'>About Me</h3>
-              <h1 className='font-anton uppercase text-[30px] leading-9 desktop:text-[72px] desktop:leading-[80px]'>Exploring <br/> The <span className='text-[#18A3DF]'>Coder&apos;s</span> Identity</h1>
+              <h3 className='font-anton uppercase text-[16px] text-[#9C9C9C] desktop:text-[34px] animate-appearSlideTop appear-timeline-thirty'>About Me</h3>
+              <h1 className='font-anton uppercase text-[30px] leading-9 desktop:text-[72px] desktop:leading-[80px] animate-appearSlideTop appear-timeline-thirty animation-delay-three'>Exploring <br/> The <span className='text-[#18A3DF]'>Coder&apos;s</span> Identity</h1>
               <div className='flex justify-center mt-5 desktop:hidden'>
                 <Image
                   src="/images/icons/shape-bg.png"
@@ -82,7 +82,7 @@ export default function Home() {
                   height={127}
                 />
               </div>
-              <div className='text-[12px] mt-5 font-monaRegular desktop:text-[18px] desktop:w-[90%]'>
+              <div className='text-[12px] mt-5 font-monaRegular desktop:text-[18px] desktop:w-[90%] animate-appearSlideTop appear-timeline-thirty animation-delay-two'>
                 <p>
                   Hi there 👋, I&apos;m <span className='text-[#18A3DF] font-monaMedium'>Yudha! </span>
                 </p>
@@ -100,7 +100,7 @@ export default function Home() {
                 </p>
               </div>
               <div className='flex gap-52 desktop:gap-[520px]'>
-                <div className='relative flex items-center mt-[-5px] ml-[-10px] desktop:mt-3'>
+                <div className='relative flex items-center mt-[-5px] ml-[-10px] desktop:mt-3 animate-appearSlideRight appear-timeline-thirty'>
                   <p className='text-[8px] border border-[#1E1E1E] rounded-full px-[10px] py-[3px] desktop:text-[10px]'>6 Projects Done</p>
                   <Image
                     className='absolute mt-[-35px] left-[85px] desktop:w-[24px] desktop:h-[28.57px] desktop:left-24 desktop:mt-[-40px]'
@@ -110,7 +110,7 @@ export default function Home() {
                     height={15}
                   />
                 </div>
-                <a href="mailto:yudhaericpamungkas@gmail.com" target='_blank' className='desktop:-mt-10'>
+                <a href="mailto:yudhaericpamungkas@gmail.com" target='_blank' className='desktop:-mt-10 animate-appearFadeIn appear-timeline-sixty'>
                   <div className='flex flex-col items-center mt-[-5px]'>
                     <Image
                       className='desktop:w-[60px] desktop:h-[60px]'
@@ -124,7 +124,7 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div className='desktop:flex items-center desktop:w-[30%] justify-center mt-5 hidden'>
+            <div className='desktop:flex items-center desktop:w-[30%] justify-center mt-5 hidden animate-appearFadeIn appear-timeline-sixty'>
               <Image
                 src="/images/icons/shape-bg.png"
                 alt="shape"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,8 +25,29 @@ const config: Config = {
       'mobile': '360px',
       'desktop': '1280px',
     },
+    keyframes: {
+      appearSlideTop: {
+        '0%': { opacity: '0', transform: 'translateX(70px)' },
+        '100%': { opacity: '1', transform: 'translateX(0px)' },
+      },
+      appearSlideRight: {
+        '0%': { opacity: '0', transform: 'translateY(50px)' },
+        '100%': { opacity: '1', transform: 'translateY(0px)' },
+      },
+      appearFadeIn: {
+        '0%': { opacity: '0' },
+        '100%': { opacity: '1' },
+      },
+      spin: {
+        '0%': { transform: 'rotate(0deg)' },
+        '100%': { transform: 'rotate(360deg)' },
+      },
+    },
     animation: {
-      'spin-slow': 'spin 4s linear infinite',
+      spinSlow: 'spin 4s linear infinite',
+      appearSlideTop: 'appearSlideTop ease-in-out',
+      appearSlideRight: 'appearSlideRight ease-in-out',
+      appearFadeIn: 'appearFadeIn ease-in-out',
     }
   },
   plugins: [],
