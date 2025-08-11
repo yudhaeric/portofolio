572e66de08a287c26d12c54ebe1fff4b26e6fc90
yudhaeric <yudhaericpamungkas@gmail.com>
2025-08-11 11:45:51 +0700

    Create Types Of Variable

---
+++ app/utils/types.tsx
@@ -0,0 +1,14 @@
+export interface SocialMedia {
+  id: number;
+  url: string;
+  icon: string;
+}
+
+export interface Project {
+  id: number;
+  image: string;
+  title: string;
+  url: string;
+  desc: string;
+  tech: string;
+}
